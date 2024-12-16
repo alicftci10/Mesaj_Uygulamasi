433491ab1f82ede53bdf9ebfd94333526f2c082a
ALi <alicftci10@gmail.com>
2024-12-16 18:08:59 +0300

    Add project files.

---
+++ Mesaj_Gonder/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Mesaj_Gonder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672"),
+                ClientProvidedName = "Mesaj Gönderici Uygulama"
+            };
+
+            using var connection = factory.CreateConnection();
+
+            using IModel channel = connection.CreateModel();
+
+            string queueName = "ornek_kuyruk";
+            channel.QueueDeclare(
+                queue: queueName,
+                durable: true,     // Kuyruk kalıcı mı?
+                exclusive: false,  // Diğer bağlantılar da kuyruğa erişebilir mi?
+                autoDelete: false  // Kuyruk kullanılmadığında silinsin mi?
+            );
+
+            string message = "Merhaba, Bu bir mesajdır!!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: queueName,
+                basicProperties: null,
+                body: body
+            );
+
+            Console.WriteLine($"Mesaj gönderildi: {message}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Mesaj_Oku/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Mesaj_Oku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672"),
+                ClientProvidedName = "Mesaj Gönderici Uygulama"
+            };
+
+            using var connection = factory.CreateConnection();
+
+            using IModel channel = connection.CreateModel();
+
+            string queueName = "ornek_kuyruk";
+            channel.QueueDeclare(
+                queue: queueName,
+                durable: true,     // Kuyruk kalıcı mı?
+                exclusive: false,  // Diğer bağlantılar da kuyruğa erişebilir mi?
+                autoDelete: false  // Kuyruk kullanılmadığında silinsin mi?
+            );
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(
+                "ornek_kuyruk",
+                true, 
+                consumer
+            );
+
+            consumer.Received += Consumer_Received;
+
+            Console.ReadLine();
+        }
+
+        private static void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+        {
+            Console.WriteLine($"Gelen Mesaj: {Encoding.UTF8.GetString(e.Body.ToArray())}");
+        }
+    }
+}
